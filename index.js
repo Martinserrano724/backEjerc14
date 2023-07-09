2474c75bba12a88bbbe70e115a046c1937161ee6
Martinserrano724 <martinserrano724@gmail.com>
2023-07-09 01:15:57 -0300

    creacion de la conecion del puerto

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-import { express } from "express"; 
-console.log('esto es un cambio 2');
+import  express  from "express"; 
+// configurar puerto
+// crear una instancia 
+
+const app = express();
+app.set("port", process.env.PORT || 4000);
+app.listen(app.get("port"), ()=>console.log('estoy en el puerto' + app.get("port"))
+);
+
