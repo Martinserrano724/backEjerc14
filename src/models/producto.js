e1f139401238426aea48cb91bf0749b775365e1a
Martinserrano724 <martinserrano724@gmail.com>
2023-07-09 21:32:18 -0300

    creacion del schema

---
+++ src/models/producto.js
@@ -0,0 +1,32 @@
+import { Schema ,model } from "mongoose";
+const productoSchema = new Schema({
+    nombreProducto: {
+        type: String,
+        minLength:2,
+        maxLength:100,
+        unique:true,
+        required:true
+
+    },
+    cantidadPlatos:{
+        type: Number,
+        min: 1,
+        max:10000,
+        required: true
+    },
+    imagen:{
+        type: String,
+        required: true
+    },
+    procesoPreracion:{
+        type:String,
+        required:true
+    },
+    ingrediente:{
+        type:String,
+        required:true
+    },
+})
+
+const Producto = model('producto',productoSchema)
+export default Producto;
